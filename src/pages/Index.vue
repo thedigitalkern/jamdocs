794a36c3156ba6f3b5761923fe220754105b7889
DRAVASP SHROFF <dravaspshroff@gmail.com>
2020-03-30 08:41:46 +0530

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,8 +5,8 @@
       <nav>
         <!-- To use other icons here, you need to import them in the Shortcut component -->
         <Shortcut link="/getting-started" text="Introduction" icon="play-icon" />
-        <Shortcut link="/theme-configuration" text="Configuration" icon="sliders-icon" />
-        <Shortcut link="/theme-configuration#changing-colors" text="Change colors" icon="eye-icon" />
+        <Shortcut link="/web-dev" text="Web Development" icon="sliders-icon" />
+        <Shortcut link="/web-dev#advertising-online-seo" text="Paid Advertising" icon="eye-icon" />
       </nav>
       <GitLink class="git" size="large" />
     </div>
