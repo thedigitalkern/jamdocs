3f011fba9105d34e066952d350004da161e0b4b7
DRAVASP SHROFF <dravaspshroff@gmail.com>
2020-03-30 09:01:21 +0530

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -39,7 +39,7 @@ export default {
     return {
       title: this.description,
       meta: [
-        { key: 'description', name: 'description', content: 'A theme for static site documentation based on Gridsome, ready to deploy to Netlify in one click.' }
+        { key: 'description', name: 'description', content: 'Educate and Empower via Digital Kern' }
       ]
     }
   }
