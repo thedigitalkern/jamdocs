07ebfb996a5d1f41203b87d9064750b9a7fcf922
DRAVASP SHROFF <dravaspshroff@gmail.com>
2020-04-03 23:17:09 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ export default function (Vue, { router, head, isClient, appOptions }) {
 
   head.meta.push({
     name: 'theme-color',
-    content: '#10c186'
+    content: '#053eff'
   })
 
   head.meta.push({
